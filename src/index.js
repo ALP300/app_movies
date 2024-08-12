1295b7d4bd0e3b085c20b5a3609a95cd8dfc4f15
ALP300 <aitorpalomino1@gmail.com>
2024-08-11 21:08:53 -0500

    conexion bd

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import{dirname, join} from 'path'
 import { fileURLToPath } from 'url'
 
 import indexRoute from './routes/index.js'
+import { Conectar } from './services/conexion.js'
 
 const app= express()
 const __dirname= dirname(fileURLToPath(import.meta.url))
@@ -11,6 +12,8 @@ app.set('views', join(__dirname,'views'))
 app.set('view engine','ejs')
 app.use(indexRoute)
 app.use(express.static(join(__dirname,'public')))
+
+Conectar()
 
 app.listen(3000)
 console.log("El servidor está siendo escuchado por el puerto 3000")
---
+++ src/services/conexion.js
@@ -0,0 +1,24 @@
+import pg from 'pg';
+const { Client }= pg;
+
+
+const config={
+    user: 'db_cine_user',
+    host: 'dpg-cqsmhjdumphs73c1ord0-a.oregon-postgres.render.com',
+    database: 'db_cine',
+    password: 'DYjI59OSB73eQnh7JTQLBiFa6BItjLfe',
+    port: 5432,
+    ssl: {
+        rejectUnauthorized: false
+    }
+}
+
+export async function Conectar() {
+    const cliente= new Client(config)
+    try{
+        await cliente.connect()
+        console.log("Conectado a la base de datos")
+    }catch(error){
+        console.log(error)
+    }
+}
