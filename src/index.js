9c2a78e9f7178b8efa3d3dd482aba322b8732171
ALP300 <aitorpalomino1@gmail.com>
2024-09-01 18:29:15 -0500

    realizar api

--- src/index.js
+++ src/index.js
@@ -1,21 +1,33 @@
-import express from 'express'
-import{dirname, join} from 'path'
-import { fileURLToPath } from 'url'
+import express from 'express';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+import cors from 'cors'; // Importar cors
 
-import indexRoute from './routes/index.js'
-import { Conectar } from './services/conexion.mjs'
+import indexRoute from './routes/index.js';
+import { Conectar } from './services/conexion.mjs';
 
-const app= express()
-const __dirname= dirname(fileURLToPath(import.meta.url))
-console.log(join(__dirname,'views'))
+const app = express();
+const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.json())
-app.set('views', join(__dirname,'views'))
-app.set('view engine','ejs')
-app.use(indexRoute)
-app.use(express.static(join(__dirname,'public')))
+// Configurar CORS
+app.use(cors({
+  origin: 'https://app-movies-5.onrender.com' // Permite solicitudes desde tu dominio desplegado
+}));
 
-Conectar()
+app.use(express.json());
+app.set('views', join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
-app.listen(3000)
-console.log("El servidor está siendo escuchado por el puerto 3000")
+// Usar rutas
+app.use(indexRoute);
+
+// Servir archivos estáticos
+app.use(express.static(join(__dirname, 'public')));
+
+// Conectar a la base de datos
+Conectar();
+
+// Iniciar el servidor
+app.listen(3000, () => {
+  console.log("El servidor está siendo escuchado por el puerto 3000");
+});
